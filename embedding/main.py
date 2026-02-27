4c19ff4422ed31cec53ec1f7ded943db9a5e9dc0
shelepuginivan <shelepuginivanm@gmail.com>
2026-02-28 00:54:44 +0300

    feat(embedding): basic kafka setup

--- embedding/main.py
+++ embedding/main.py
@@ -1,5 +1,20 @@
+import json
+
+from kafka import KafkaConsumer, KafkaProducer
+
+
+producer = KafkaProducer(value_serializer=lambda v: json.dumps(v).encode("utf-8"))
+
+
+def consume_embedding():
+    consumer = KafkaConsumer("embedding.queue")
+
+    for msg in consumer:
+        print(msg.value)
+
+
 def main():
-    print("Hello from embedding!")
+    consume_embedding()
 
 
 if __name__ == "__main__":
