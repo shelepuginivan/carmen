e59f7ab7baeaae6749b3c6c9b91e7c366e7b6fde
shelepuginivan <shelepuginivanm@gmail.com>
2026-02-28 22:49:33 +0300

    feat(embedding): add search configuration

--- embedding/src/models/config.py
+++ embedding/src/models/config.py
@@ -7,5 +7,7 @@ class Config(BaseSettings):
     kafka_consumer_group: str = "embedding-consumer-group"
     kafka_topic_chunks_queue: str = "chunks.queue"
     kafka_topic_chunks_ready: str = "chunks.ready"
+    kafka_topic_search_requests: str = "search.requests"
+    kafka_topic_search_responses: str = "search.responses"
 
     model_config = SettingsConfigDict(env_prefix="CARMEN_EMBEDDING_")
