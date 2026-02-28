5583c2a5b5152b28d7b79f55979bd01d8a627231
shelepuginivan <shelepuginivanm@gmail.com>
2026-02-28 22:50:15 +0300

    feat(embedding): launch multiple adapters

--- embedding/src/main.py
+++ embedding/src/main.py
@@ -1,19 +1,24 @@
 import logging
+from concurrent.futures import ProcessPoolExecutor
 
 from sentence_transformers import SentenceTransformer
 
 from adapters.chunks import ChunkAdapter
+from adapters.search import SearchAdapter
 from models.config import Config
 
 
 def main():
     logging.basicConfig(level=logging.INFO)
     config = Config()  # type: ignore
-
     transformer = SentenceTransformer(config.sentence_transformer)
 
-    adapter = ChunkAdapter(config, transformer)
-    adapter.handle()
+    chunk_adapter = ChunkAdapter(config, transformer)
+    search_adapter = SearchAdapter(config, transformer)
+
+    with ProcessPoolExecutor(max_workers=2) as executor:
+        executor.submit(chunk_adapter.handle)
+        executor.submit(search_adapter.handle)
 
 
 if __name__ == "__main__":
