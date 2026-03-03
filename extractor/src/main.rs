25431a70d606983ae3e750183cda04bfde34e3d5
shelepuginivan <shelepuginivanm@gmail.com>
2026-03-03 07:14:26 +0300

    feat(extractor): add basic s3 flow

--- extractor/src/main.rs
+++ extractor/src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Ok;
+use extractor::config::Config;
+use s3::{Bucket, Region, creds::Credentials};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let config = Config::read_from_env()?;
+
+    let region = Region::Custom {
+        region: config.s3_region,
+        endpoint: config.s3_endpoint,
+    };
+
+    let credentials = Credentials::new(
+        Some(&config.s3_access_key),
+        Some(&config.s3_secret_key),
+        None,
+        None,
+        None,
+    )?;
+
+    let bucket = Bucket::new(&config.s3_bucket, region, credentials)?.with_path_style();
+
+    let s3_path = "test.file";
+    let test = b"I'm going to S3!";
+
+    let response_data = bucket.put_object(s3_path, test).await?;
+    assert_eq!(response_data.status_code(), 200);
+
+    Ok(())
 }
