ac432e666fa85d053dde0497dcd67ffa21912215
shelepuginivan <shelepuginivanm@gmail.com>
2026-03-17 00:14:29 +0300

    feat(search): implement s3 document storage

---
+++ search/pkg/search/document_storage.go
@@ -0,0 +1,42 @@
+package search
+
+import (
+	"context"
+	"io"
+
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/shelepuginivan/carmen/search/pkg/config"
+)
+
+type DocumentStorage struct {
+	client *s3.Client
+	bucket string
+}
+
+func NewDocumentStorage(cfg *config.Config) *DocumentStorage {
+	client := s3.New(s3.Options{
+		Region:       cfg.S3Region,
+		Credentials:  credentials.NewStaticCredentialsProvider(cfg.S3AccessKey, cfg.S3SecretKey, ""),
+		BaseEndpoint: &cfg.S3Endpoint,
+		UsePathStyle: true,
+	})
+
+	return &DocumentStorage{
+		client: client,
+		bucket: cfg.S3Bucket,
+	}
+}
+
+func (ds *DocumentStorage) GetDocument(ctx context.Context, key string) ([]byte, error) {
+	res, err := ds.client.GetObject(ctx, &s3.GetObjectInput{
+		Bucket: &ds.bucket,
+		Key:    &key,
+	})
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	return io.ReadAll(res.Body)
+}
