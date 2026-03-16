b8f13539ab37722d9d89c35d3b2a062393da72a3
shelepuginivan <shelepuginivanm@gmail.com>
2026-03-17 02:55:36 +0300

    refactor(search): move workspace repo to repositories

---
+++ search/pkg/repository/workspace.go
@@ -0,0 +1,80 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/shelepuginivan/carmen/search/pkg/dal"
+	"github.com/shelepuginivan/carmen/search/pkg/model"
+	"gorm.io/gorm"
+)
+
+type WorkspaceRepository struct {
+	db *gorm.DB
+	s3 *dal.S3
+}
+
+func NewWorkspace(db *gorm.DB, s3 *dal.S3) *WorkspaceRepository {
+	return &WorkspaceRepository{
+		db: db,
+		s3: s3,
+	}
+}
+
+func (wr *WorkspaceRepository) CreateWorkspace(ctx context.Context, name string, description string) error {
+	return wr.db.WithContext(ctx).Create(&model.Workspace{
+		Name:        name,
+		Description: description,
+	}).Error
+}
+
+func (wr *WorkspaceRepository) GetWorkspace(ctx context.Context, identifier string) (*model.Workspace, error) {
+	var workspace model.Workspace
+
+	res := wr.db.
+		WithContext(ctx).
+		Where("id = ?", identifier).
+		Or("name = ?", identifier).
+		First(&workspace)
+
+	return &workspace, res.Error
+}
+
+func (wr *WorkspaceRepository) ListWorkspaces(ctx context.Context) ([]*model.Workspace, error) {
+	var workspaces []*model.Workspace
+
+	res := wr.db.
+		WithContext(ctx).
+		Select("name", "description").
+		Order("name").Find(&workspaces)
+
+	return workspaces, res.Error
+}
+
+func (wr *WorkspaceRepository) DeleteWorkspace(ctx context.Context, identifier string) error {
+	var workspace model.Workspace
+
+	err := wr.db.
+		WithContext(ctx).
+		Preload("Documents", func(db *gorm.DB) *gorm.DB {
+			return db.Select("object_key")
+		}).
+		Where("id = ?").
+		Or("name = ?", identifier).
+		First(&workspace).
+		Error
+	if err != nil {
+		return err
+	}
+
+	for _, document := range workspace.Documents {
+		wr.s3.DeleteDocument(ctx, document.ObjectKey)
+	}
+
+	return wr.db.
+		WithContext(ctx).
+		Unscoped().
+		Where("id = ?", identifier).
+		Or("name = ?", identifier).
+		Delete(&model.Workspace{}).
+		Error
+}
