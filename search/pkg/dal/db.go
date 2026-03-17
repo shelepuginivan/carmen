d3fe267c8265483a097ec375fdcaec62f2a178be
shelepuginivan <shelepuginivanm@gmail.com>
2026-03-17 23:42:09 +0300

    feat(search): db connection backoff

--- search/pkg/dal/db.go
+++ search/pkg/dal/db.go
@@ -2,15 +2,46 @@
 package dal
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/shelepuginivan/carmen/search/pkg/config"
 	"github.com/shelepuginivan/carmen/search/pkg/model"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
+func connectWithBackoff(
+	cfg *config.Config,
+	initDelay time.Duration,
+	delayScaleFactor int,
+	retries int,
+) (*gorm.DB, error) {
+	var (
+		dsn    = cfg.PostgresDSN()
+		delay  = initDelay
+		factor = time.Duration(delayScaleFactor)
+
+		db  *gorm.DB
+		err error
+	)
+
+	for range retries {
+		db, err = gorm.Open(postgres.Open(dsn))
+		if err == nil {
+			return db, nil
+		}
+
+		time.Sleep(delay)
+		delay *= factor
+	}
+
+	return nil, fmt.Errorf("db: connection failed after %d retries: %w", retries, err)
+}
+
 // NewDBConnection connects to the database and performs necessary setup.
 func NewDBConnection(cfg *config.Config) (*gorm.DB, error) {
-	db, err := gorm.Open(postgres.Open(cfg.PostgresDSN()), &gorm.Config{})
+	db, err := connectWithBackoff(cfg, time.Second, 2, 5)
 	if err != nil {
 		return nil, err
 	}
