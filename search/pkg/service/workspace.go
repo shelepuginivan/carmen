509691f1390945493fbaf0ba3ebd469c6704c88b
shelepuginivan <shelepuginivanm@gmail.com>
2026-03-17 22:12:17 +0300

    feat(search): add workspace service

---
+++ search/pkg/service/workspace.go
@@ -0,0 +1,32 @@
+package service
+
+import (
+	"context"
+
+	"github.com/shelepuginivan/carmen/search/pkg/model"
+	"github.com/shelepuginivan/carmen/search/pkg/repository"
+)
+
+type WorkspaceService struct {
+	repo *repository.WorkspaceRepository
+}
+
+func NewWorkspace(repo *repository.WorkspaceRepository) *WorkspaceService {
+	return &WorkspaceService{repo}
+}
+
+func (ws *WorkspaceService) CreateWorkspace(ctx context.Context, name string, description string) error {
+	return ws.repo.CreateWorkspace(ctx, name, description)
+}
+
+func (ws *WorkspaceService) GetWorkspace(ctx context.Context, identifier string) (*model.Workspace, error) {
+	return ws.repo.GetWorkspace(ctx, identifier)
+}
+
+func (ws *WorkspaceService) ListWorkspaces(ctx context.Context) ([]*model.Workspace, error) {
+	return ws.repo.ListWorkspaces(ctx)
+}
+
+func (ws *WorkspaceService) DeleteWorkspace(ctx context.Context, identifier string) error {
+	return ws.repo.DeleteWorkspace(ctx, identifier)
+}
