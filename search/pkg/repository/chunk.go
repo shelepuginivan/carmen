9c250a8730e15f854d4af7fb50d59d8909384afd
shelepuginivan <shelepuginivanm@gmail.com>
2026-03-24 00:38:31 +0300

    feat(search): add base chunk repository

---
+++ search/pkg/repository/chunk.go
@@ -0,0 +1,38 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/pgvector/pgvector-go"
+	"github.com/shelepuginivan/carmen/search/pkg/model"
+	"gorm.io/gorm"
+)
+
+type ChunksRepository struct {
+	db *gorm.DB
+}
+
+func NewChunk(db *gorm.DB) *ChunksRepository {
+	return &ChunksRepository{db}
+}
+
+func (cr *ChunksRepository) Create(
+	ctx context.Context,
+	documentID string,
+	text string,
+	embedding []float32,
+) (*model.Chunk, error) {
+	chunk := model.Chunk{
+		DocumentID: documentID,
+		Text:       text,
+		Embedding:  pgvector.NewVector(embedding),
+	}
+
+	err := cr.db.WithContext(ctx).Create(&chunk).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return &chunk, nil
+
+}
