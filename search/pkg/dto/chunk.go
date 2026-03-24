12e011c62aa29777b133895317b60b9c7118298f
shelepuginivan <shelepuginivanm@gmail.com>
2026-03-25 02:14:32 +0300

    feat(search): add search DTOs

--- search/pkg/dto/chunk.go
+++ search/pkg/dto/chunk.go
@@ -5,3 +5,9 @@ type ChunkReady struct {
 	Text       string    `json:"text"`
 	Embedding  []float32 `json:"embedding"`
 }
+
+type ChunkMetadata struct {
+	ID         string `json:"id"`
+	DocumentID string `json:"document_id"`
+	Text       string `json:"text"`
+}
--- search/pkg/dto/search.go
+++ search/pkg/dto/search.go
@@ -3,3 +3,8 @@ package dto
 type SemanticSearchResponse struct {
 	Embedding []float32 `json:"embedding"`
 }
+
+type SearchRequest struct {
+	Query string `form:"q" binding:"required,min=1"`
+	Limit int    `form:"limit,default=5" binding:"min=1"`
+}
