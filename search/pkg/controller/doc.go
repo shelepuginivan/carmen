454c862bca752be980e1dc353dc6ceb67ea8ed2c
shelepuginivan <shelepuginivanm@gmail.com>
2026-03-25 12:09:32 +0300

    docs(search): add general api info docs

--- search/pkg/controller/doc.go
+++ search/pkg/controller/doc.go
@@ -1,2 +1,8 @@
 // Package controller provides service API controllers.
 package controller
+
+// @title Carmen
+// @version 1.0
+// @description Hybrid search platform
+// @license.name MIT
+// @license.url https://github.com/shelepuginivan/carmen/blob/main/LICENSE
