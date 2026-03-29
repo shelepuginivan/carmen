65f21af4504bd45b24c111d3e4516cb4db971783
shelepuginivan <shelepuginivanm@gmail.com>
2026-03-29 21:49:15 +0300

    feat(langdetector): provide language detection api

--- langdetector/src/main.py
+++ langdetector/src/main.py
@@ -1,10 +1,21 @@
-from fastapi import FastAPI, Body
+from fastapi import Depends, FastAPI, Body, HTTPException
 from fastapi.responses import PlainTextResponse
+from lingua import LanguageDetector
+
+from config import Config, get_config
+from detector import get_language_detector
 
 
 app = FastAPI()
 
 
 @app.post("/detect", response_class=PlainTextResponse)
-def read_item(body: str = Body(media_type="text/plain")):
-    return body
+def detect_language(
+    text: str = Body(media_type="text/plain"),
+    config: Config = Depends(get_config),
+    detector: LanguageDetector = Depends(get_language_detector),
+):
+    lang = detector.detect_language_of(text) or config.get_fallback_language()
+    if lang is None:
+        raise HTTPException(status_code=404, detail="cannot detect language reliably")
+    return lang.name.lower()
