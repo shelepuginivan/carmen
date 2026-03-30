9ce9d3e38140d309e154de574225401e0bd3b443
shelepuginivan <shelepuginivanm@gmail.com>
2026-03-30 20:10:55 +0300

    feat(search): add langdetector service

---
+++ search/pkg/service/langdetector.go
@@ -0,0 +1,47 @@
+package service
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+)
+
+type LangdetectorService struct {
+	client *http.Client
+	url    string
+}
+
+func NewLangdetector(url string) *LangdetectorService {
+	transport := &http.Transport{
+		MaxIdleConns:        10,
+		MaxIdleConnsPerHost: 10,
+		IdleConnTimeout:     30 * time.Second,
+		DisableKeepAlives:   false,
+	}
+	client := &http.Client{
+		Transport: transport,
+		Timeout:   30 * time.Second,
+	}
+
+	return &LangdetectorService{client, url}
+}
+
+func (lds *LangdetectorService) DetectLanguage(text string) (string, error) {
+	res, err := lds.client.Post(lds.url, "text/plain", bytes.NewBufferString(text))
+	if err != nil {
+		return "", err
+	}
+
+	if res.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("cannot detect language")
+	}
+
+	lang, err := io.ReadAll(res.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(lang), nil
+}
