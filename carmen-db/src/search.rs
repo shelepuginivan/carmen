5ed41d295032d892af4ca757b4051cdc6eb26230
shelepuginivan <shelepuginivanm@gmail.com>
2026-06-18 03:31:02 +0300

    feat(db): fts

--- carmen-db/src/search.rs
+++ carmen-db/src/search.rs
@@ -5,6 +5,31 @@ use sqlx::types::Uuid;
 use crate::chunks::Chunk;
 
 impl Chunk {
+    pub async fn full_text_search(
+        pool: &PgPool,
+        collection_id: Uuid,
+        query: &str,
+        language: &str,
+        limit: i32,
+    ) -> sqlx::Result<Vec<Self>> {
+        sqlx::query_as(
+            r#"
+            SELECT chunks.id, document_id, text, language::text
+            FROM chunks
+            JOIN documents ON documents.id = document_id
+            WHERE documents.collection_id = $1
+            ORDER BY ts_rank_cd(fts_vector, plainto_tsquery($2::regconfig, $3)) DESC
+            LIMIT $4
+            "#,
+        )
+        .bind(collection_id)
+        .bind(language)
+        .bind(query)
+        .bind(limit)
+        .fetch_all(pool)
+        .await
+    }
+
     pub async fn semantic_search(
         pool: &PgPool,
         collection_id: Uuid,
@@ -18,13 +43,13 @@ impl Chunk {
             SELECT chunks.id, document_id, text, language::text
             FROM chunks
             JOIN documents ON documents.id = document_id
-            WHERE documents.collection_id = $2
-            ORDER BY $1 <=> embedding
+            WHERE documents.collection_id = $1
+            ORDER BY $2 <=> embedding
             LIMIT $3
             "#,
         )
-        .bind(embedding)
         .bind(collection_id)
+        .bind(embedding)
         .bind(limit)
         .fetch_all(pool)
         .await
