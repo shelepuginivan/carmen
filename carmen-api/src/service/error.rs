eb9c4705031399d9004b3736410313c5f0796d69
shelepuginivan <shelepuginivanm@gmail.com>
2026-06-21 07:46:32 +0300

    chore(api): fix clippy warnings

--- carmen-api/src/service/error.rs
+++ carmen-api/src/service/error.rs
@@ -11,8 +11,8 @@ pub enum Error {
     #[error("an internal storage error occurred")]
     Storage(#[from] StorageError),
 
-    #[error("{0}")]
-    NLP(#[from] carmen_nlp::Error),
+    #[error("an internal server error occurred")]
+    Nlp(#[from] carmen_nlp::Error),
 
     #[error("an internal server error occurred")]
     Anyhow(#[from] anyhow::Error),
