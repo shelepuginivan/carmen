8574775290657234a0f41c0ebcfb73dac9cc4240
shelepuginivan <shelepuginivanm@gmail.com>
2026-07-05 09:30:19 +0300

    db: remove `anyhow` dependency

--- carmen-db/src/migrate.rs
+++ carmen-db/src/migrate.rs
@@ -1,7 +1,9 @@
+use std::error::Error;
+
 use log::info;
 
 #[tokio::main]
-async fn main() -> anyhow::Result<()> {
+async fn main() -> Result<(), Box<dyn Error>> {
     env_logger::init_from_env("CARMEN_LOG");
 
     let pool = carmen_db::connect_from_env().await?;
