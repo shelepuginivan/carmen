0762900e291b447f07a337496710e75e44c1609e
shelepuginivan <shelepuginivanm@gmail.com>
2026-07-19 00:00:22 +0300

    storage/drivers: add s3 driver

--- carmen-storage/src/drivers/mod.rs
+++ carmen-storage/src/drivers/mod.rs
@@ -3,18 +3,21 @@ use std::path::Path;
 use enum_dispatch::enum_dispatch;
 
 use crate::error::Result;
+use crate::stream::Stream;
 
-// TODO: come up with a solution for byte stream responses.
-type ResponseDataStream = s3::request::ResponseDataStream;
+mod s3;
+
+use s3::S3;
 
 #[enum_dispatch]
 pub trait StorageDriver {
     async fn get_object_as_string(&self, id: &str) -> Result<String>;
-    async fn get_object_as_stream(&self, id: &str) -> Result<ResponseDataStream>;
+    async fn get_object_as_stream(&self, id: &str) -> Result<Stream>;
     async fn put_object_from_local_file(&self, id: &str, path: &Path) -> Result<()>;
-    async fn delete_object(&self, id: &str) -> Result<()>;
     async fn delete_many_objects(&self, ids: &[&str]) -> Result<()>;
 }
 
 #[enum_dispatch(StorageDriver)]
-pub enum Driver {}
+pub enum Driver {
+    S3,
+}
---
+++ carmen-storage/src/drivers/s3.rs
@@ -0,0 +1,54 @@
+use std::path::Path;
+
+use s3::creds::Credentials;
+use s3::serde_types::ObjectIdentifier;
+use s3::{Bucket, Region};
+use tokio::fs::File;
+
+use crate::drivers::StorageDriver;
+use crate::env::read_env;
+use crate::error::Result;
+use crate::stream::Stream;
+
+#[derive(Clone, Debug)]
+pub struct S3 {
+    bucket: Box<Bucket>,
+}
+
+impl S3 {
+    pub fn new_from_env() -> Result<Self> {
+        let endpoint = read_env("CARMEN_S3_ENDPOINT")?;
+        let region = read_env("CARMEN_S3_REGION")?;
+        let bucket = read_env("CARMEN_S3_BUCKET")?;
+        let access_key = read_env("CARMEN_S3_ACCESS_KEY")?;
+        let secret_key = read_env("CARMEN_S3_SECRET_KEY")?;
+
+        let region = Region::Custom { region, endpoint };
+        let credentials = Credentials::new(Some(&access_key), Some(&secret_key), None, None, None)?;
+        let bucket = Bucket::new(&bucket, region, credentials)?.with_path_style();
+
+        Ok(Self { bucket })
+    }
+}
+
+impl StorageDriver for S3 {
+    async fn get_object_as_string(&self, id: &str) -> Result<String> {
+        Ok(self.bucket.get_object(id).await?.to_string()?)
+    }
+
+    async fn get_object_as_stream(&self, id: &str) -> Result<Stream> {
+        Ok(Stream::S3(self.bucket.get_object_stream(id).await?.bytes))
+    }
+
+    async fn put_object_from_local_file(&self, id: &str, path: &Path) -> Result<()> {
+        let mut file = File::open(path).await?;
+        self.bucket.put_object_stream(&mut file, id).await?;
+        Ok(())
+    }
+
+    async fn delete_many_objects(&self, ids: &[&str]) -> Result<()> {
+        let objects: Vec<_> = ids.into_iter().map(|s| ObjectIdentifier::new(*s)).collect();
+        self.bucket.delete_objects(objects).await?;
+        Ok(())
+    }
+}
