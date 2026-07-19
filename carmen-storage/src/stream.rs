6d8d3f25d37b20a8c7e31912515fe80ea0e561ea
shelepuginivan <shelepuginivanm@gmail.com>
2026-07-19 03:13:09 +0300

    storage: feature `axum` for stream to body conversion

--- carmen-storage/src/stream.rs
+++ carmen-storage/src/stream.rs
@@ -6,3 +6,13 @@ pub enum Stream {
     FS(ReaderStream<File>),
     S3(DataStream),
 }
+
+#[cfg(feature = "axum")]
+impl Into<axum::body::Body> for Stream {
+    fn into(self) -> axum::body::Body {
+        match self {
+            Self::FS(s) => axum::body::Body::from_stream(s),
+            Self::S3(s) => axum::body::Body::from_stream(s),
+        }
+    }
+}
